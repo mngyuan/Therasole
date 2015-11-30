1b7375f7531f751771323883aabe042c9d117f87
Kevin Lee <kevinmlee.x@gmail.com>
2015-11-30 11:21:15 -0800

    added somewhat unnecessary <title/> changes for views

--- fitsole.js
+++ fitsole.js
@@ -1,14 +1,15 @@
 Tasks = new Mongo.Collection("tasks");
 
+Router.configure({
+  layoutTemplate: 'main',
+  title: 'FitSole',
+});
 Router.route('/', {
   name     : 'landing',
-  template : 'landing'
+  template : 'landing',
 });
-Router.configure({
-  layoutTemplate: 'main'
-});
-Router.route('/heatmap');
-Router.route('/balance');
-Router.route('/graphs');
+Router.route('/heatmap', { title: 'Heatmap | Fitsole' });
+Router.route('/balance', { title: 'Balance | Fitsole' });
+Router.route('/graphs', { title: 'Graphs | Fitsole' });
 // Router.route('/share');
-Router.route('/settings');
+Router.route('/settings', { title: 'Settings | Fitsole' });
