7b875cc5063b710d3b8858c32e404ff4861216a8
Kevin Lee <kevinmlee.x@gmail.com>
2015-11-30 11:17:56 -0800

    a half broken eggtimer implementation

--- client/client.js
+++ client/client.js
@@ -2,6 +2,12 @@
 // if (Meteor.isClient) {
 // golly isn't meteor such dandy software?
 
+Template.eggtimer.helpers({
+  marks: [ '12am', '1am', '2am', '3am', '4am', '5am', '6am', '7am', '8am',
+           '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm',
+           '6pm', '7pm', '8pm', '9pm', '10pm', '11pm' ]
+});
+
 Template.landing.helpers({
   tasks: _ => {
     if (Session.get("hideCompleted")) {
