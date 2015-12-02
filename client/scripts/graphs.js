4d55ccf6434f8b0326d8ad78813c66b077b836f0
Kevin Lee <kevinmlee.x@gmail.com>
2015-12-01 21:40:36 -0800

    refactor to have a scripts directory

--- client/scripts/graphs.js
+++ client/scripts/graphs.js
@@ -1,13 +1,3 @@
-// implicit:
-// if (Meteor.isClient) {
-// golly isn't meteor such dandy software?
-
-Template.eggtimer.helpers({
-  marks: [ '12am', '1am', '2am', '3am', '4am', '5am', '6am', '7am', '8am',
-           '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm',
-           '6pm', '7pm', '8pm', '9pm', '10pm', '11pm' ]
-});
-
 Template.barchart.onRendered(_ => {
   var mockdata = `State,Under 5 Years,5 to 13 Years,14 to 17 Years,18 to 24 Years,25 to 44 Years,45 to 64 Years,65 Years and Over
 AL,310504,552339,259034,450818,1231572,1215966,641667
@@ -149,45 +139,4 @@ WY,38253,60890,29314,53980,137338,147279,65614`;
       .attr("dy", ".35em")
       .style("text-anchor", "end")
       .text(function(d) { return d; });
-});
-
-Template.landing.helpers({
-  tasks: _ => {
-    if (Session.get("hideCompleted")) {
-      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 }});
-    } else {
-      return Tasks.find({}, { sort: { createdAt: -1 } });
-    }
-  },
-  hideCompleted: function() {
-    return Session.get("hideCompleted");
-  }
-});
-
-Template.landing.events({
-  "submit .new-task": event => {
-    event.preventDefault();
-    var text = event.target.text.value;
-
-    Tasks.insert({
-      text,
-      createdAt: new Date()
-    });
-
-    event.target.text.value = "";
-  },
-  "change .hide-completed input": event => {
-    Session.set("hideCompleted", event.target.checked);
-  }
-});
-
-Template.task.events({
-  "click .toggle-checked": function() {
-    Tasks.update(this._id, {
-      $set: { checked: ! this.checked }
-    });
-  },
-  "click .delete": function() {
-    Tasks.remove(this._id);
-  }
 });
