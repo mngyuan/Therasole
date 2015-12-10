fdaa7d43f823446b023a25024a21683974e0f7ef
Kevin Lee <kevinmlee.x@gmail.com>
2015-12-10 14:00:39 -0800

    add scale, fix sensor locations

--- client/scripts/settings.js
+++ client/scripts/settings.js
@@ -5,21 +5,32 @@ Template.settings.helpers({
 });
 
 Template.settings.events({
-  "keypress .new-task": event => {
-    if (event.which === 13) {
-      event.preventDefault();
+  "submit .new-task": event => {
+    event.preventDefault();
+    console.log(event);
 
-      MockData.insert({
-        timestamp: event.target.text.value,
-        data1: event.target.data1.value,
-        data2: event.target.data2.value,
-        data3: event.target.data3.value,
-        data4: event.target.data4.value,
-        data5: event.target.data5.value,
-        data6: event.target.data6.value,
-        createdAt: new Date(),
-      });
-    }
+    // for (var i = 0; i < 40; i++) {
+    //   MockData.insert({
+    //     timestamp: event.target.text.value,
+    //     data1: Math.floor(Math.random()*5) + 3,
+    //     data2: Math.floor(Math.random()*5) + 3,
+    //     data3: Math.floor(Math.random()*5) + 3,
+    //     data4: Math.floor(Math.random()*5) + 3,
+    //     data5: Math.floor(Math.random()*5) + 3,
+    //     data6: Math.floor(Math.random()*5) + 3,
+    //     createdAt: new Date(),
+    //   });
+    // }
+    MockData.insert({
+      timestamp: event.target.text.value,
+      data1: event.target.data1.value,
+      data2: event.target.data2.value,
+      data3: event.target.data3.value,
+      data4: event.target.data4.value,
+      data5: event.target.data5.value,
+      data6: event.target.data6.value,
+      createdAt: new Date(),
+    });
   },
 });
 
